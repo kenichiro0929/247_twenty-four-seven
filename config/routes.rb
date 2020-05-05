e7f79edb3bf74049c7c9533e6842516257bfe4d9
手島健一郎 <teshimakenichirou@teshimakenichirounoMacBook-Pro.local>
2020-05-05 17:37:54 +0900

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get '/signup', to:'users#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   root 'static_pages#home'
- # get  'static_pages/home'
-
+  get '/signup', to:'users#new'
+  post '/signup', to:'users#create'
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
+  resources :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
