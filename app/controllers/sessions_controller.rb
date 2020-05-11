76d7fe0ea583825bc0c3e1e2bf5be9889f33c13c
手島健一郎 <teshimakenichirou@teshimakenichirounoMacBook-Pro.local>
2020-05-11 19:41:29 +0900

    modify sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
-			if user.activate?
+			if user.activated?
 				log_in user
 				params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 				redirect_back_or user
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 				redirect_to root_url
 			end
 		else
-			flash,now[:danger] = 'Invalid email/password combination'
+			flash.now[:danger] = 'Invalid email/password combination'
 			render 'new'
 		end
 	end
